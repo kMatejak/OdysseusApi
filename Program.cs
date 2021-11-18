779dab96b942e265e0809284ca1a02337f74c77d
kMatejak <krzysztof.matejak@moovem.pro>
2021-11-18 14:21:47 +0100

    feature/add crud for api

--- Program.cs
+++ Program.cs
@@ -1,34 +1,78 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+builder.Services.AddDbContext<OdyssDb>(opt => opt.UseInMemoryDatabase("OdyssList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapGet("/", () => "Type three-letter destination code after domain adrress in URL bar, for example 'odyss-api.com/PAN'");
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapGet("/{destination}", async (string destination, OdyssDb db) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var route = await db.Routes.Where(d => d.Destination == destination).FirstAsync();
+    var response = "{" + "\n" + "\tdestination: \'" + route.Destination?.ToString() + "\',\n" + "\tlist: " + route.List?.ToString() + "\n" + "}";
 
-app.MapGet("/weatherforecast", () =>
+    return response;
+});
+
+app.MapGet("/routes", async (OdyssDb db) =>
+    await db.Routes.ToListAsync());
+
+
+app.MapGet("/routes/{id}", async (int id, OdyssDb db) =>
+    await db.Routes.FindAsync(id)
+        is Route route
+            ? Results.Ok(route)
+            : Results.NotFound());
+
+app.MapPost("/routes", async (Route route, OdyssDb db) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
+    db.Routes.Add(route);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/destinations/{route.Id}", route);
+});
+
+app.MapPut("/routes/{id}", async (int id, Route inputRoute, OdyssDb db) =>
+{
+    var route = await db.Routes.FindAsync(id);
+
+    if (route is null) return Results.NotFound();
+
+    route.Destination = inputRoute.Destination;
+    route.List = inputRoute.List;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+
+app.MapDelete("/routes/{id}", async (int id, OdyssDb db) =>
+{
+    if (await db.Routes.FindAsync(id) is Route destination)
+    {
+        db.Routes.Remove(destination);
+        await db.SaveChangesAsync();
+        return Results.Ok(destination);
+    }
+
+    return Results.NotFound();
 });
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+class Route
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public string? Destination { get; set; }
+    public string? List { get; set; }
+}
+
+class OdyssDb : DbContext
+{
+    public OdyssDb(DbContextOptions<OdyssDb> options)
+        : base(options) { }
+
+    public DbSet<Route> Routes => Set<Route>();
 }
